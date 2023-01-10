e51e12e6b00f52d73b8cecb1ab9582c93908b83f
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-10 03:12:13 -0800

    refactor: use Default export for auth routes

---
+++ server/src/routes/auth/index.ts
@@ -0,0 +1,4 @@
+export { default as loginRoute } from './loginRoute';
+export { default as logoutRoute } from './logoutRoute';
+export { default as refreshRoute } from './refreshRoute';
+export { default as registerRoute } from './registerRoute';
