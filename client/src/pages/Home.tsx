b14be809151d98e97a105246b55625cca4fd87aa
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-10 16:07:05 -0800

    refactor: post fetch test in home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,42 @@
-import { Text } from '@nextui-org/react';
+import { Text, Loading, Card } from '@nextui-org/react';
+import { useGetTopPostsQuery } from 'redux/posts/postsSlice';
+import { parseISO, formatDistanceToNow } from 'date-fns';
 
 function Home() {
+  const { data, isLoading } = useGetTopPostsQuery(null);
+
+  if (isLoading) {
+    return <Loading size='xl' />;
+  }
+
+  const { post: posts } = data;
+
   return (
     <div>
-      <Text h1>Hello WORLDWWD!</Text>
+      <Text css={{ textAlign: 'center ' }} h1>
+        Posts:
+      </Text>
+      {posts.map((post: Post) => (
+        <Card
+          css={{
+            mt: '4rem',
+            mw: '400px',
+            mx: 'auto'
+          }}
+          key={post.postid}
+        >
+          <Card.Header>
+            <Text b>{post.postedBy}</Text>
+          </Card.Header>
+          <Card.Divider />
+          <Card.Body>
+            <Text>{post.postText}</Text>
+          </Card.Body>
+          <Card.Footer>
+            <Text>{formatDistanceToNow(parseISO(post.created_at))}</Text>
+          </Card.Footer>
+        </Card>
+      ))}
     </div>
   );
 }
