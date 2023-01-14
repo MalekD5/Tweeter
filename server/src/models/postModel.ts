ab480355e41ff5b0d4f0a941375cc8680c68e5ba
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-14 13:00:33 -0800

    refactor(postModel): extract queries to constants

--- server/src/models/postModel.ts
+++ server/src/models/postModel.ts
@@ -1,5 +1,5 @@
 import { RowDataPacket } from 'mysql2';
-import { pool, postsTable } from '../Connection';
+import { pool, postsTable } from '../MySQLConnection';
 import crypto from 'crypto';
 
 interface Post extends RowDataPacket {
@@ -9,25 +9,22 @@ interface Post extends RowDataPacket {
   created_at: Date;
 }
 
-export async function createPost(postedBy: number, postText: string) {
+const INSERT_POST = `INSERT INTO ${postsTable}(postId, postText, postedBy) VALUES(?,?,?)`;
+const SELECT_TOP_10_POSTS = `SELECT * FROM ${postsTable} ORDER BY created_at DESC LIMIT 10;`;
+
+export async function createPost(
+  postedBy: number,
+  postText: string
+): Promise<string> {
   const postId = crypto.randomUUID().replace(/-/g, '');
 
-  await pool
-    .promise()
-    .execute(
-      `INSERT INTO ${postsTable}(postId, postText, postedBy) VALUES(?,?,?)`,
-      [postId, postText, postedBy]
-    );
+  await pool.promise().execute(INSERT_POST, [postId, postText, postedBy]);
 
   return postId;
 }
 
 export async function retrieveTopPosts(): Promise<Post[]> {
-  const [posts] = await pool
-    .promise()
-    .query<Post[]>(
-      `SELECT * FROM ${postsTable} ORDER BY created_at DESC LIMIT 10;`
-    );
+  const [posts] = await pool.promise().query<Post[]>(SELECT_TOP_10_POSTS);
   return posts;
 }
 
