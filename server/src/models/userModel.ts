9d2a1da8f2e32ed238dee7353ec586ae63077409
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-14 13:04:05 -0800

    refactor/fix: extract queries to constant & fix sql syntax * add verified type string * make create function return last inserted id

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,13 +1,14 @@
-import { pool, authTable } from '../Connection';
+import { pool, authTable } from '../MySQLConnection';
 import { RowDataPacket } from 'mysql2';
 
-interface ResponseUser extends RowDataPacket {
-  id: number;
-  username: string;
-  email: string;
-  password: string;
-  refreshToken: string;
-}
+interface ResponseUser extends RowDataPacket, User {}
+
+const SELECT_USER_BY_EMAIL = `SELECT * FROM ${authTable} WHERE email=?`;
+const SELECT_USER_BY_REFRESH_TK = `SELECT * FROM ${authTable} WHERE refreshToken=?`;
+const SELECT_USERNAME_OR_EMAIL = `SELECT email,username FROM ${authTable} WHERE email=? OR username=?`;
+
+const UPDATE_USER = `UPDATE ${authTable} SET refreshToken=? WHERE email=?`;
+const INSERT_USER = `INSERT INTO ${authTable}(username, email, password) VALUES(?,?,?);`;
 
 class User {
   id: number;
@@ -15,37 +16,33 @@ class User {
   email: string;
   password: string;
   refreshToken: string;
+  verified: string;
 
   constructor(
     id: number,
     email: string,
     username: string,
     password: string,
-    refreshToken: string
+    refreshToken: string,
+    verified: string
   ) {
     this.id = id;
     this.email = email;
     this.username = username;
     this.password = password;
     this.refreshToken = refreshToken;
+    this.verified = verified;
   }
 
   async save() {
-    await pool
-      .promise()
-      .execute(`UPDATE ${authTable} SET refreshToken=? WHERE email=?`, [
-        this.refreshToken,
-        this.email
-      ]);
+    await pool.promise().execute(UPDATE_USER, [this.refreshToken, this.email]);
   }
 }
 
 export async function findByEmail(email: string): Promise<User | undefined> {
   const [users] = await pool
     .promise()
-    .execute<ResponseUser[]>(`SELECT * FROM ${authTable} WHERE email=?`, [
-      email
-    ]);
+    .execute<ResponseUser[]>(SELECT_USER_BY_EMAIL, [email]);
 
   if (!users || users.length === 0) return undefined;
 
@@ -56,7 +53,8 @@ export async function findByEmail(email: string): Promise<User | undefined> {
     user.email,
     user.username,
     user.password,
-    user.refreshToken
+    user.refreshToken,
+    user.verified
   );
 }
 
@@ -65,10 +63,7 @@ export async function findByRefreshToken(
 ): Promise<User | undefined> {
   const [users] = await pool
     .promise()
-    .execute<ResponseUser[]>(
-      `SELECT * FROM ${authTable} WHERE refreshToken=?`,
-      [refreshToken]
-    );
+    .execute<ResponseUser[]>(SELECT_USER_BY_REFRESH_TK, [refreshToken]);
 
   if (!users || users.length === 0) return undefined;
 
@@ -79,7 +74,8 @@ export async function findByRefreshToken(
     user.email,
     user.username,
     user.password,
-    user.refreshToken
+    user.refreshToken,
+    user.verified
   );
 }
 
@@ -89,10 +85,7 @@ export async function checkDuplicate(
 ): Promise<{ email: string; username: string } | undefined> {
   const [users] = await pool
     .promise()
-    .execute<ResponseUser[]>(
-      `SELECT email,username FROM ${authTable} WHERE email=? OR username=?`,
-      [email, username]
-    );
+    .execute<ResponseUser[]>(SELECT_USERNAME_OR_EMAIL, [email, username]);
 
   const [user] = users;
   if (user) {
@@ -106,12 +99,10 @@ export async function create(
   email: string,
   password: string
 ) {
-  await pool
+  const data: any = await pool
     .promise()
-    .execute(
-      `INSERT INTO ${authTable}(username, email, password) VALUES(?,?,?)`,
-      [username, email, password]
-    );
+    .execute(INSERT_USER, [username, email, password]);
+  return data[0].insertId;
 }
 
 export type { User };
