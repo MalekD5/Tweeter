c3e99cd319d498c1717d77ba61a863ebf94690ff
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-14 12:56:07 -0800

    refactor: add exports for alert and notfound components

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Layout } from './Layout';
 export { default as AuthMiddleware } from './middleware/AuthMiddlware';
 export { default as PersistLoginMiddleware } from './middleware/PersistLoginMiddleware';
+export { default as NotFound } from './NotFound';
+export { default as Alert } from './Alert';
