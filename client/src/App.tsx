1d86558bc73e3ab2a54efc247126b5567c1aae90
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-14 12:58:38 -0800

    feat: add 404 route, wrap pages with layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
-import { Layout, AuthMiddleware, PersistLoginMiddleware } from './components';
+import {Layout, AuthMiddleware, PersistLoginMiddleware, NotFound} from './components';
 import { Home, Login, Register } from 'pages';
 import Protected from './pages/Protected';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
 
-        <Route element={<PersistLoginMiddleware />}>
-          <Route element={<AuthMiddleware />}>
-            <Route path='/protected' element={<Protected />} />
+      <Route element={<PersistLoginMiddleware />}>
+        <Route element={<AuthMiddleware />}>
+          <Route element={<Layout />}>
+              <Route path='/protected' element={<Protected />} />
           </Route>
         </Route>
       </Route>
+
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
