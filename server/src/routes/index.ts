d523c4efdd4ef7f4dfccf11c30da2bc4087fe16a
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-15 07:50:49 -0800

    fix: add missing verify route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
-import { registerRoute, loginRoute, logoutRoute, refreshRoute } from './auth';
+import {
+  registerRoute,
+  loginRoute,
+  logoutRoute,
+  refreshRoute,
+  verifyRouter
+} from './auth';
 import postRouter from './posts/postsRouter';
-import { authMiddleware } from '../middleware/authMiddleware';
+import { authMiddleware } from '@/middleware/authMiddleware';
 const router = express.Router();
 
 // auth routes
@@ -9,6 +15,7 @@ router.use('/', registerRoute);
 router.use('/', loginRoute);
 router.use('/', refreshRoute);
 router.use('/', logoutRoute);
+router.use('/', verifyRouter);
 
 // routes added after this middleware requires authentication (access token)
 router.use(authMiddleware);
