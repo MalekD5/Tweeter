05768fc809c8c570f9a15a6d646acdafa28f5e51
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-15 07:51:01 -0800

    fix: add missing export for verify route

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -2,3 +2,4 @@ export { default as loginRoute } from './loginRoute';
 export { default as logoutRoute } from './logoutRoute';
 export { default as refreshRoute } from './refreshRoute';
 export { default as registerRoute } from './registerRoute';
+export { default as verifyRouter } from './verify.route';
