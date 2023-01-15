2b8262b7fd90722f5577d52f9f039e1739e7e5a8
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-15 07:50:11 -0800

    fix: post slice not unable to fetch due to missing token

--- client/src/redux/features/posts/postsSlice.ts
+++ client/src/redux/features/posts/postsSlice.ts
@@ -1,8 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://localhost:5000/api/v1/post',
+  credentials: 'include',
+  prepareHeaders(headers, { getState }) {
+    const token = (getState() as any).auth.token;
+    if (token) headers.set('authorization', `Bearer ${token}`);
+    return headers;
+  }
+})
 export const postApi = createApi({
   reducerPath: 'posts',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1/post' }),
+  baseQuery,
   endpoints: (builder) => ({
     getTopPosts: builder.query({
       query: () => '/'
