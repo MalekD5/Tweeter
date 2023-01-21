6ce2dc526c15002b5a04239c438120b66b45b353
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-21 09:50:52 -0800

    refactor: navigate to explore instead of protected in login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,7 +5,8 @@ import {
   Button,
   Input,
   Checkbox,
-  Spacer, Modal
+  Spacer,
+  Modal
 } from '@nextui-org/react';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -17,7 +18,7 @@ import { useNavigate } from 'react-router-dom';
 import validator from 'validator';
 import style from '@/styles/form.module.css';
 import { useState } from 'react';
-import { Alert } from "@/components";
+import { Alert } from '@/components';
 
 const loginSchema = z.object({
   email: z.string().refine(validator.isEmail),
@@ -52,83 +53,92 @@ function Login() {
         })
       );
       localStorage.setItem('persist', persist);
-      navigate('/protected', { replace: true });
+      navigate('/explore', { replace: true });
     } catch (err: any) {
       if ('originalStatus' in err && err?.originalStatus === 403) {
         setError('please verify your email.');
       } else {
-        setError('An Internal server error has occured.')
+        setError('An Internal server error has occured.');
       }
     }
   };
 
   const closeHandler = () => {
     setVisible(false);
-  }
+  };
 
   return (
     <Container
-        display='flex'
-        direction='column'
-        alignItems='center'
-        justify='center'
-        css={{gap: '1rem'}}
+      display='flex'
+      direction='column'
+      alignItems='center'
+      justify='center'
+      css={{ gap: '1rem' }}
     >
-      <Spacer y={1}/>
+      <Spacer y={1} />
       <Button onPress={() => setVisible(true)}>Login</Button>
       <Modal closeButton open={visible} onClose={closeHandler}>
         <Modal.Header>
-          <Text h1>
-            Login
-          </Text>
+          <Text h1>Login</Text>
         </Modal.Header>
         <Modal.Body>
           <form className={style.container}>
-            <Alert error={error}/>
+            <Alert error={error} />
             <Input
-                label='Email'
-                clearable
-                bordered
-                size='lg'
-                type='email'
-                placeholder='email@email.com'
-                {...register('email')}
-                aria-label='email'
+              label='Email'
+              clearable
+              bordered
+              size='lg'
+              type='email'
+              placeholder='email@email.com'
+              {...register('email')}
+              aria-label='email'
             />
             <Input.Password
-                label='Password'
-                bordered
-                clearable
-                type='password'
-                size='lg'
-                placeholder='password'
-                {...register('password')}
-                aria-label='password'
-                css={{m: '0'}}
+              label='Password'
+              bordered
+              clearable
+              type='password'
+              size='lg'
+              placeholder='password'
+              {...register('password')}
+              aria-label='password'
+              css={{ m: '0' }}
             />
 
             <Controller
-                render={({field}) => (
-                    <Checkbox size='sm' {...field} color='gradient'>
-                      Remember this device
-                    </Checkbox>
-                )}
-                control={control}
-                name='persist'
-                defaultValue={false}
+              render={({ field }) => (
+                <Checkbox size='sm' {...field} color='gradient'>
+                  Remember this device
+                </Checkbox>
+              )}
+              control={control}
+              name='persist'
+              defaultValue={false}
             />
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <div style={{
-            display: 'flex',
-            margin: '0 auto',
-            gap: '1rem'
-          }}>
-            <Button size="lg" auto flat color="error" onPress={closeHandler}>
+          <div
+            style={{
+              display: 'flex',
+              margin: '0 auto',
+              gap: '1rem'
+            }}
+          >
+            <Button size='lg' auto flat color='error' onPress={closeHandler}>
               Close
             </Button>
-            <Button size="lg" auto flat color="success" type='submit' onClick={submit(handleSubmit)}>Login</Button>
+            <Button
+              size='lg'
+              auto
+              flat
+              color='success'
+              type='submit'
+              onClick={submit(handleSubmit)}
+            >
+              Login
+            </Button>
           </div>
         </Modal.Footer>
       </Modal>
