2ed049fcb43106131f1b8a4c8acb17843d5c73a7
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-21 06:47:10 -0800

    refactor: imports in component index.ts

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Layout } from './Layout';
-export { default as AuthMiddleware } from './middleware/AuthMiddlware';
-export { default as PersistLoginMiddleware } from './middleware/PersistLoginMiddleware';
+export { default as AuthMiddleware } from './middleware/AuthMiddleware';
 export { default as NotFound } from './NotFound';
 export { default as Alert } from './Alert';
