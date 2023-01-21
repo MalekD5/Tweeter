decceadbf04d53712c75debcae52512fb6289afe
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-21 09:43:50 -0800

    refactor: add logout button to sidebar * some style changes

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -4,11 +4,26 @@ import { IconContext } from 'react-icons';
 import { FiBookmark, FiUser } from 'react-icons/fi';
 import { HiHome } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useLogoutMutation } from '@/redux/features/auth/authServiceSlice';
+import { AiOutlineTwitter } from 'react-icons/ai';
 
 function Sidebar() {
+  const navigate = useNavigate();
+  const [logoutRequest] = useLogoutMutation();
+  const handleLogout = async () => {
+    await logoutRequest(null);
+    navigate('/login');
+  };
+
   return (
     <IconContext.Provider value={{ size: '2rem' }}>
       <div className={styles.sidebar_container}>
+        <div className={styles.sidebar_item}>
+          <Link to='/explore'>
+            <AiOutlineTwitter />
+          </Link>
+        </div>
         <SidebarItem>
           <HiHome />
           <SidebarText>Home</SidebarText>
@@ -24,6 +39,11 @@ function Sidebar() {
         <div className={styles.sidebar_item}>
           <Button rounded>
             <Text size='17px'>Tweet</Text>
+          </Button>
+        </div>
+        <div className={`${styles.sidebar_item} ${styles.logout_button}`}>
+          <Button onPress={handleLogout} color='error' rounded>
+            <Text size='17px'>Logout</Text>
           </Button>
         </div>
       </div>
