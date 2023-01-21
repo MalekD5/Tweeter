a5a7b4a39c63a87f1f9039d5aa89a132eee7b12a
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-21 09:46:13 -0800

    refactor: add components to layout.tsx

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,13 +2,17 @@ import { Outlet } from 'react-router-dom';
 import Header from '@/components/Sidebar';
 import styles from '@/styles/layout.module.css';
 import Sidebar from './Sidebar';
+import Search from './Search';
 
 function Layout() {
   return (
     <>
       <div className={styles.container}>
         <Sidebar />
-        <Outlet />
+        <Search />
+        <div className={styles.outlet_container}>
+          <Outlet />
+        </div>
       </div>
     </>
   );
