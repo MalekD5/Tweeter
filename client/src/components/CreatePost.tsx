6379d5b3a3afb80c836ab1225d3de7b38740d1c7
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-21 09:44:10 -0800

    style: make create post a bit smaller

--- client/src/components/CreatePost.tsx
+++ client/src/components/CreatePost.tsx
@@ -1,40 +1,43 @@
-import { Textarea, Text, Button } from "@nextui-org/react";
-import { useForm } from "react-hook-form";
-import { zodResolver} from "@hookform/resolvers/zod";
+import { Textarea, Text, Button } from '@nextui-org/react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import validator from "validator";
-import { useCreatePostMutation} from "@/redux/features/posts/postsSlice";
+import validator from 'validator';
+import { useCreatePostMutation } from '@/redux/features/posts/postsSlice';
 
 const postSchema = z.object({
-    postText: z.string().trim().min(1).max(280).transform(validator.escape),
-})
+  postText: z.string().trim().min(1).max(280).transform(validator.escape)
+});
 
 function CreatePost() {
-    const { handleSubmit, register } = useForm({
-        resolver: zodResolver(postSchema)
-    });
+  const { handleSubmit, register } = useForm({
+    resolver: zodResolver(postSchema)
+  });
 
-    const [ createPostMutation ] = useCreatePostMutation();
+  const [createPostMutation] = useCreatePostMutation();
 
-    const onSubmit = async (data: any) => {
-        console.log(data.postText);
-        try {
-            const response = await createPostMutation({ postText: data.postText }).unwrap();
-            console.log(response)
-        } catch (err: any) {
-            console.log(err);
-        }
-    };
+  const onSubmit = async (data: any) => {
+    console.log(data.postText);
+    try {
+      const response = await createPostMutation({
+        postText: data.postText
+      }).unwrap();
+      console.log(response);
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <div>
-            <Text h3 b>Create a Post:</Text>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Textarea {...register('postText')} />
-                <Button type='submit'>Post</Button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Textarea placeholder='what is happening?' {...register('postText')} />
+        <Button auto type='submit'>
+          Post
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;
