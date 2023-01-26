793f9026bcde4fb9f4b767f929d27c9842bf9701
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-01-26 11:41:40 -0800

    update Search.tsx

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,10 +1,14 @@
 import styles from '@/styles/layout.module.css';
 import CreatePost from './CreatePost';
+import { Avatar, Textarea } from '@nextui-org/react';
 
 function Search() {
   return (
     <div className={styles.search_container}>
-      <CreatePost />
+      <div className={styles.search_tweet}>
+        <Avatar src='' />
+        <Textarea underlined placeholder='what is happening?' />
+      </div>
     </div>
   );
 }
