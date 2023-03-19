e94104ba009da17ec66b96b33720ffa0151f876b
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-03-19 09:55:44 -0700

    refactor: change react-router routes structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import { Layout, AuthMiddleware, NotFound } from './components';
-import { Explore, Login, Register } from 'pages';
+import { Landing, Explore, Settings } from '@/pages';
+import Authenticated from '@/features/auth/Authenticated';
+import { Layout } from '@/components';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-
-      <Route element={<AuthMiddleware />}>
+      <Route path='/' element={<Landing />} />
+      <Route element={<Authenticated />}>
         <Route element={<Layout />}>
           <Route path='/explore' element={<Explore />} />
+          <Route path='/settings' element={<Settings />} />
         </Route>
       </Route>
-
-      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
