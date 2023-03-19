fd1a5063c17daeb316454fd1158ac658540efeb7
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-03-19 10:05:45 -0700

    refactor: response to array in get tweets controller

--- server/src/controllers/tweet/latestTweets.controller.ts
+++ server/src/controllers/tweet/latestTweets.controller.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from 'express';
-import type { Tweet } from '../../models/tweetModel';
-import { retrieveTopTweets } from '../../models/tweetModel';
+import { retrieveTopTweets } from '@/models/tweetModel';
 
 export default async function latestPostsController(
-  req: Request,
+  _req: Request,
   res: Response
 ) {
   try {
-    const tweet: Tweet[] = await retrieveTopTweets();
-    res.status(200).json({ tweet });
+    const tweet = await retrieveTopTweets();
+    res.status(200).json(tweet);
   } catch (err: any) {
-    console.error(err);
+    console.log(err);
     res.status(500).json({ error: (err as Error).message });
   }
 }
