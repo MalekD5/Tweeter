cadf7388ca9fdc621ef27ea2de6dc577c5e1d789
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-03-19 09:33:18 -0700

    refactor: remove next-ui code switched to daisyui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { NextUIProvider, createTheme } from '@nextui-org/react';
+import store from './app/store';
 import { Provider } from 'react-redux';
-import { store } from 'redux/Store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './main.css';
-
-const theme = createTheme({
-  type: 'dark'
-});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <NextUIProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/*' element={<App />} />
-          </Routes>
-        </BrowserRouter>
-      </NextUIProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
