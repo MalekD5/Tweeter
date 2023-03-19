d4f6c5e2f38553aab367a4b6ea02eb6a85ad80c7
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-03-19 09:57:31 -0700

    refactor: move determineColor function to Button file

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -1,5 +1,3 @@
-import { determineColor } from '@/utils/ComponentUtils';
-
 export interface ButtonProps
   extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'className'> {
   color?:
@@ -26,6 +24,23 @@ function button(props: ButtonProps) {
       {children}
     </button>
   );
+}
+
+function determineColor(color: string | undefined) {
+  switch (color) {
+    case 'danger':
+      return 'bg-red-700 hover:bg-red-500 disabled:bg-red-900 disabled:text-gray-300';
+    case 'success':
+      return 'bg-green-700 hover:bg-green-500 disabled:bg-green-900 disabled:text-gray-300';
+    case 'warning':
+      return 'bg-yellow-700 hover:bg-yellow-500 disabled:bg-yellow-900 disabled:text-gray-300';
+    case 'white':
+      return 'bg-white !text-black font-bold hover:bg-gray-300';
+    case 'bordered-default':
+      return 'bg-inherit border border-blue-200 hover:bg-blue-300';
+    default:
+      return 'bg-blue-700 disabled:bg-blue-900 disabled:text-gray-300';
+  }
 }
 
 export default button;
