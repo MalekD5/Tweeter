e1ac4105bff352e48fbb85d9d35197c5836d0530
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-03-19 10:01:56 -0700

    refactor: include username in top tweets sql

--- server/src/models/tweetModel.ts
+++ server/src/models/tweetModel.ts
@@ -1,16 +1,19 @@
 import { RowDataPacket } from 'mysql2';
-import { pool, tweetsTable } from '../MySQLConnection';
+import { pool, tweetsTable, authTable } from '../MySQLConnection';
+import { parseISO, formatDistanceToNow } from 'date-fns';
 import crypto from 'crypto';
 
 interface Tweet extends RowDataPacket {
   tweetid: string;
   tweetedBy: number;
   tweetText: string;
-  created_at: Date;
+  created_at: string;
+  pfp: string;
+  username?: string;
 }
 
 const INSERT_TWEET = `INSERT INTO ${tweetsTable}(tweetId, tweetText, tweetedBy) VALUES(?,?,?)`;
-const SELECT_TOP_10_TWEETS = `SELECT * FROM ${tweetsTable} ORDER BY created_at DESC LIMIT 10;`;
+const SELECT_TOP_10_TWEETS = `SELECT t.tweetid,t.tweetedBy,t.tweetText,t.created_at,a.pfp,a.username FROM ${tweetsTable} as t,${authTable} as a WHERE t.tweetedBy=a.id ORDER BY created_at DESC LIMIT 10;`;
 const EDIT_TWEET = `UPDATE ${tweetsTable} SET tweetText=? WHERE tweetId=?`;
 const VERIFY_OWNERSHIP = `SELECT tweetedBy FROM ${tweetsTable} WHERE tweetid=?`;
 
@@ -24,7 +27,13 @@ export async function createTweet(tweetedBy: number, tweetText: string) {
 
 export async function retrieveTopTweets() {
   const [tweets] = await pool.promise().query<Tweet[]>(SELECT_TOP_10_TWEETS);
-  return tweets;
+  return tweets.map((t) => ({
+    id: t.tweetid,
+    text: t.tweetText,
+    date: formatDistanceToNow(parseISO(new Date(t.created_at).toISOString())),
+    pfp: t.pfp,
+    username: t.username
+  }));
 }
 
 export async function editTweet(tweetId: string, tweetText: string) {
