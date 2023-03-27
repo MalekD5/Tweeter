b89ccefb84d1893445045d27f2134b2271d821b1
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-03-27 15:37:51 -0700

    refactor: make RowDataPacket a type import

--- server/src/models/tweetModel.ts
+++ server/src/models/tweetModel.ts
@@ -1,4 +1,4 @@
-import { RowDataPacket } from 'mysql2';
+import type { RowDataPacket } from 'mysql2';
 import { pool, tweetsTable, authTable } from '../MySQLConnection';
 import { parseISO, formatDistanceToNow } from 'date-fns';
 import crypto from 'crypto';
