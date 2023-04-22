86df70d008473c4634e09a48556755602da20673
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-22 11:59:42 -0700

    fix: imports in pages folder

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,3 +1,7 @@
 export { default as Landing } from './Landing';
 export { default as Explore } from './Explore';
 export { default as Settings } from './Settings';
+export { default as Bookmarks } from './Bookmarks';
+export { default as Profile } from './Profile';
+export { default as Login } from './flow/Login';
+export { default as Register } from './flow/Register';
