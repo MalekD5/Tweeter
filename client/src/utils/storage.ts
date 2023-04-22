d6a74829753b7b4db2ac4e676cf824c7d3033925
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-22 11:56:13 -0700

    feat: logic to handle local storage

---
+++ client/src/utils/storage.ts
@@ -0,0 +1,70 @@
+import { z } from 'zod';
+
+const userStoreSchema = z.object({
+  username: z.string().min(5).max(25).trim(),
+  displayname: z.string().min(5).max(40).trim(),
+  pfp: z.string().url().optional()
+});
+
+type UserStoreType = z.infer<typeof userStoreSchema>;
+
+export function handleToken(token: string) {
+  const persist = localStorage.getItem('persist');
+  if (persist === 'true') {
+    localStorage.setItem('token', token);
+  } else {
+    sessionStorage.setItem('token', token);
+  }
+}
+
+export function handleLocalStorage(
+  token: string,
+  username: string,
+  displayname: string,
+  persist: boolean,
+  pfp?: string
+) {
+  if (persist) {
+    localStorage.setItem('token', token);
+  } else {
+    sessionStorage.setItem('token', token);
+  }
+  localStorage.setItem('persist', persist.toString());
+
+  const userStore = userStoreSchema.parse({ username, displayname, pfp });
+  localStorage.setItem('userStore', JSON.stringify(userStore));
+}
+
+export function useUserStore() {
+  const userStore = localStorage.getItem('userStore');
+  if (userStore === null)
+    return { success: false, error: 'user store does not exists' };
+
+  try {
+    const parsedStore = JSON.parse(userStore);
+    const result = userStoreSchema.parse(parsedStore);
+    return {
+      success: true,
+      result
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error
+    };
+  }
+}
+
+export function saveToStore(data: UserStoreType) {
+  const parse = userStoreSchema.safeParse(data);
+  if (!parse.success) {
+    return {
+      success: false,
+      error: parse.error
+    };
+  }
+  localStorage.setItem('userStore', JSON.stringify(parse.data));
+  return {
+    success: true
+  };
+}
