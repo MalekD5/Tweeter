ed443bad887afe4008944d9de676cabf2660b6c5
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-22 11:58:56 -0700

    refactor: add routes and providers to main & App files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
-import { Landing, Explore, Settings } from '@/pages';
-import Authenticated from '@/features/auth/Authenticated';
-import { Layout } from '@/components';
+import {
+  Landing,
+  Explore,
+  Settings,
+  Bookmarks,
+  Profile,
+  Login,
+  Register
+} from '@/pages';
+import { Layout, Authenticated } from '@/components';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Landing />} />
+      <Route path='i/flow/login' element={<Login />} />
+      <Route path='i/flow/register' element={<Register />} />
       <Route element={<Authenticated />}>
         <Route element={<Layout />}>
           <Route path='/explore' element={<Explore />} />
           <Route path='/settings' element={<Settings />} />
+          <Route path='/bookmarks' element={<Bookmarks />} />
+          <Route path='/profile' element={<Profile />} />
         </Route>
       </Route>
     </Routes>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import store from './redux/store';
-import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MantineProvider, type MantineTheme } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './api/api';
 import './main.css';
+
+const theme: Partial<MantineTheme> = {
+  colorScheme: 'dark',
+  focusRing: 'auto',
+  components: {
+    Input: {
+      styles: {
+        input: {
+          backgroundColor: '#000',
+          fontSize: '1.1rem',
+          padding: '1.5rem 0.5rem'
+        }
+      }
+    },
+    Modal: {
+      styles: {
+        root: {
+          backgroundColor: '#000'
+        },
+        body: {
+          backgroundColor: '#000'
+        },
+        content: {
+          borderRadius: '1rem'
+        },
+        header: {
+          backgroundColor: '#000'
+        }
+      }
+    }
+  }
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+        <ModalsProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/*' element={<App />} />
+            </Routes>
+          </BrowserRouter>
+        </ModalsProvider>
+      </MantineProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
