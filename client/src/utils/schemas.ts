40d448fd1c0a7527bcbf4395cabc93e02d4c3a8e
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-22 11:57:43 -0700

    refactor: add settings schema & confirmPassword to register

--- client/src/utils/schemas.ts
+++ client/src/utils/schemas.ts
@@ -14,7 +14,9 @@ export const registerSchema = z
   .object({
     email,
     password,
-    confirmPassword: password
+    confirmPassword: password,
+    username: z.string().refine(validator.isAlphanumeric),
+    displayname: z.string().refine(validator.isAlphanumeric)
   })
   .superRefine((data, ctx) => {
     if (data.password !== data.confirmPassword) {
@@ -25,3 +27,14 @@ export const registerSchema = z
       });
     }
   });
+
+export const settingSchema = z.object({
+  username: z.string().min(5).max(25).trim().refine(validator.isAlphanumeric),
+  displayname: z
+    .string()
+    .min(5)
+    .max(40)
+    .trim()
+    .refine(validator.isAlphanumeric),
+  bio: z.string().min(0).max(280).trim().default('')
+});
