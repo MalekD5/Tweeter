bfccaa2055c5dc18813a2b11be93d62aba7c39d5
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-22 11:49:43 -0700

    build: remove daisyui options from tailwind config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -15,22 +15,5 @@ module.exports = {
       }
     }
   },
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#661AE6',
-          secondary: '#D926AA',
-          accent: '#1FB2A5',
-          neutral: '#191D24',
-          'base-100': '#000',
-          info: '#3ABFF8',
-          success: '#36D399',
-          warning: '#FBBD23',
-          error: '#F87272'
-        }
-      }
-    ]
-  },
-  plugins: [require('daisyui')]
+  plugins: []
 };
