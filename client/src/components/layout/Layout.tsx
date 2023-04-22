540336479cdcabb81c1d2e3becf59fe28b705643
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-22 11:55:47 -0700

    refactor: add error boundary to layout

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from '..';
 import { SidebarProvider } from '@/context/SidebarContext';
+import { ErrorBoundary } from 'react-error-boundary';
+import Fallback from '../Fallback';
 function Layout() {
   return (
     <SidebarProvider>
       <Sidebar />
-      <main className='w-[71%] h-full ml-[29%]'>
-        <Outlet />
+      <main className='w-[93%] ml-[7%] lg:w-[71%] h-full lg:ml-[29%]'>
+        <ErrorBoundary FallbackComponent={Fallback}>
+          <Outlet />
+        </ErrorBoundary>
       </main>
     </SidebarProvider>
   );
