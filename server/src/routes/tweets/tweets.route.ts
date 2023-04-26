421519dd40e3b2b31137777f54c5a969f051982e
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-26 10:30:40 -0700

    refactor: changed /tweet route structure GET /tweet/id/:id -> specific tweet GET /tweet/user -> user tweets GET /tweet/explore -> explore page tweets POST /tweet -> creates a tweet DELETE /tweet -> deletes a tweet

--- server/src/routes/tweets/tweets.route.ts
+++ server/src/routes/tweets/tweets.route.ts
@@ -1,36 +1,37 @@
 import express from 'express';
-import { body, query } from 'express-validator';
+import { ValidationChain, body, query, param } from 'express-validator';
 import { validationMiddleware } from '@/middleware/validationMiddleware';
 import {
   createTweetController,
   latestTweetsController,
   UserTweetsController,
-  deleteTweetController
+  deleteTweetController,
+  IdController
 } from '@/controllers/tweet';
 
 const router = express.Router();
+
+const validateId = (chain: ValidationChain) =>
+  chain.trim().isAlphanumeric().isLength({ max: 36 });
 
 const tweetTextValidator = body('text')
   .trim()
   .isLength({ min: 1, max: 280 })
   .withMessage('text value length must be at least 1 or at most 280');
 
-const tweetIdValidator = body('tweet_id')
-  .trim()
-  .isLength({ max: 36 })
-  .isAlphanumeric();
+const tweetIdParam = validateId(param('id')).withMessage(
+  'Id paramter is required'
+);
 
-const tweetIdValidQuery = query('id')
-  .trim()
-  .isLength({ max: 36 })
-  .isAlphanumeric();
+const tweetIdValidQuery = validateId(query('id'));
 
 router
   .route('/')
   .post(tweetTextValidator, validationMiddleware, createTweetController)
-  .get(latestTweetsController)
   .delete(tweetIdValidQuery, validationMiddleware, deleteTweetController);
 
+router.get('/id/:id', tweetIdParam, validationMiddleware, IdController);
 router.get('/user', UserTweetsController);
+router.get('/explore', latestTweetsController);
 
 export default router;
