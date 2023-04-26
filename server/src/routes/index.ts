3c7d580c44c66877d1655e5cd85d0a7a1a3e30cf
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-26 03:33:16 -0700

    fix: add missing routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import postRouter from './tweets/tweets.route';
+import tweetRouter from './tweets/tweets.route';
+import retweetRouter from './tweets/retweets.route';
+import likeRoute from './tweets/likes.route';
 import uploadRouter from './upload/upload.route';
 import bookmarkRoute from './bookmark/bookmark.route';
 import profileRoute from './profile/profile.route';
-import likeRoute from './tweets/likes.route';
-
 import {
   registerRoute,
   loginRoute,
@@ -26,8 +26,9 @@ router.use('/', existsRoute);
 // routes added after this middleware requires authentication (access token)
 router.use(authMiddleware);
 router.use('/upload', uploadRouter);
-router.use('/tweet', postRouter);
+router.use('/tweet', tweetRouter);
 router.use('/tweet/like', likeRoute);
+router.use('/retweet', retweetRouter);
 router.use('/bookmarks', bookmarkRoute);
 router.use('/profile', profileRoute);
 
