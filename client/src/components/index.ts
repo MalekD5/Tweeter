4fecfb79dbc9755a9b6c8e49c4b6f2c281a2896b
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-26 03:24:11 -0700

    fix: missing import for SignedOut in components

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Sidebar } from './sidebar/Sidebar';
 export { default as Layout } from './layout/Layout';
 export { default as TweetCard } from './tweets/TweetCard';
 export { default as Authenticated } from './Authenticated';
+export { default as SignedOut } from './SignedOut';
