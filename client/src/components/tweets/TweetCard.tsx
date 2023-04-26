ba2ca0baf029eedfd3d947a77dd5601271d31dff
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-26 10:29:14 -0700

    refactor: remove extracted components + fix hover effect

--- client/src/components/tweets/TweetCard.tsx
+++ client/src/components/tweets/TweetCard.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import type { LatestTweetsType } from '@common/types/Endpoints';
+import type { IconType } from 'react-icons';
+import type { ElementType } from '../Dropdown';
+import { useState } from 'react';
 import {
   AiFillHeart,
   AiOutlineHeart,
@@ -6,30 +9,25 @@ import {
   AiOutlineShareAlt
 } from 'react-icons/ai';
 import { FaRegComment } from 'react-icons/fa';
-import { FiDelete, FiMoreHorizontal } from 'react-icons/fi';
-import { BiBookmarkMinus, BiBookmarkPlus } from 'react-icons/bi';
+import { FiMoreHorizontal } from 'react-icons/fi';
 import { useHover } from '@mantine/hooks';
 import { useMutation } from 'react-query';
 import {
-  DeleteTweet,
   LikeTweet,
   UnlikeTweet,
   createRetweet,
   removeRetweet
 } from '@/api/tweetAPI';
 import { queryClient } from '@/api/api';
-import { BookmarkTweet, UnBookmarkTweet } from '@/api/bookmarkAPI';
-import type { LatestTweetsType } from '@common/types/Endpoints';
-import type { IconType } from 'react-icons';
-import Dropdown, { type ElementType } from '../Dropdown';
 import { Text } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import CommentModal from './CommentModal';
+import TweetOptions from './TweetOptions';
 
 type TweetProps = {
   tweet: LatestTweetsType;
   showRetweets?: boolean;
 };
-
-type TweetOptionButtonProps = { onClick: () => void } & React.PropsWithChildren;
 
 // Hover - icon - text
 const colors = {
@@ -49,18 +47,12 @@ type TweetButtonProps = {
   ActiveIcon?: IconType;
 };
 
-type TweetOptionsProps = Pick<LatestTweetsType, 'isBookmarked' | 'isAuthor'> & {
-  reference: ElementType;
-  open: boolean;
-  id: string;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-};
-
 function TweetCardV3(props: TweetProps) {
   const { tweet } = props;
 
   const [open, setOpen] = useState(false);
   const [reference, setReference] = useState<ElementType>(null);
+  const [comment, handlers] = useState(false);
 
   const onSuccess = () => {
     queryClient.invalidateQueries(['Tweet'], { exact: true });
@@ -118,79 +110,86 @@ function TweetCardV3(props: TweetProps) {
   };
 
   return (
-    <div className='py-3 px-4 w-full border-t border-t-bordergray border-b border-b-bordergray'>
-      {tweet.isRetweeted && props.showRetweets && (
-        <div className='ml-5 flex gap-3 items-center w-fit'>
-          <AiOutlineRetweet className='text-md fill-textgray' />
-          <Text className='text-sm text-textgray hover:underline hover:cursor-pointer'>
-            You Retweeted
-          </Text>
-        </div>
-      )}
-      <div className='flex w-full gap-2'>
-        {/* img */}
-        <img
-          crossOrigin='anonymous'
-          src={tweet.pfp || 'defaultpfp.png'}
-          alt='tweet profile picture'
-          className='h-14 w-14 rounded-full bg-white mt-2'
-        />
-        <div className='basis-10/12 flex flex-col w-full py-2'>
-          <div className='flex justify-between items-center'>
-            <div className='flex gap-1 text-sm'>
-              <span className='font-semibold'>{tweet.displayname}</span>
-              <span className='text-textgray'>
-                @{tweet.username} · {tweet.created_at}
-              </span>
+    <>
+      <CommentModal open={comment} setOpen={handlers} replying_to={tweet.id} />
+      <div className='py-3 px-4 w-full border-t border-t-bordergray border-b border-b-bordergray hover:bg-white/20 hover:cursor-pointer'>
+        <Link to={`/tweet/${tweet.id}`}>
+          {tweet.isRetweeted && props.showRetweets && (
+            <div className='ml-5 flex gap-3 items-center w-fit'>
+              <AiOutlineRetweet className='text-md fill-textgray' />
+              <Text className='text-sm text-textgray hover:underline hover:cursor-pointer'>
+                You Retweeted
+              </Text>
             </div>
-            <button
-              className='z-20'
-              onClick={() => setOpen((v) => !v)}
-              ref={setReference}
-            >
-              <FiMoreHorizontal />
-            </button>
+          )}
+          <div className='flex w-full gap-2'>
+            {/* img */}
+            <img
+              crossOrigin='anonymous'
+              src={tweet.pfp || 'defaultpfp.png'}
+              alt='tweet profile picture'
+              className='h-14 w-14 rounded-full bg-white mt-2'
+            />
+            <div className='basis-10/12 flex flex-col w-full py-2'>
+              <div className='flex justify-between items-center'>
+                <div className='flex gap-1 text-sm'>
+                  <span className='font-semibold'>{tweet.displayname}</span>
+                  <span className='text-textgray'>
+                    @{tweet.username} · {tweet.created_at}
+                  </span>
+                </div>
+                <button
+                  className='z-20'
+                  onClick={() => setOpen((v) => !v)}
+                  ref={setReference}
+                >
+                  <FiMoreHorizontal />
+                </button>
 
-            <TweetOptions
-              reference={reference}
-              open={open}
-              setOpen={setOpen}
-              isAuthor={tweet.isAuthor}
-              isBookmarked={tweet.isBookmarked}
-              id={tweet.id}
-            />
-          </div>
+                <TweetOptions
+                  reference={reference}
+                  open={open}
+                  setOpen={setOpen}
+                  isAuthor={tweet.isAuthor}
+                  isBookmarked={tweet.isBookmarked}
+                  id={tweet.id}
+                />
+              </div>
 
-          <p className='mt-0.5 w-11/12 break-words text-sm font-normal'>
-            {tweet.text}
-          </p>
-          <div className='w-9/12 mt-3 flex justify-between items-center'>
-            <TweetButton
-              onClick={() => {}}
-              Icon={FaRegComment}
-              count={tweet.comments}
-            />
-            <TweetButton
-              onClick={handleRetweetButton}
-              Icon={AiOutlineRetweet}
-              count={tweet.retweets}
-              active={tweet.isRetweeted}
-              ActiveIcon={AiOutlineRetweet}
-              color='green'
-            />
-            <TweetButton
-              onClick={handleLikeButton}
-              active={tweet.isLiked}
-              ActiveIcon={AiFillHeart}
-              Icon={AiOutlineHeart}
-              count={tweet.likes}
-              color='red'
-            />
-            <TweetButton onClick={() => {}} Icon={AiOutlineShareAlt} />
+              <p className='mt-0.5 w-11/12 break-words text-sm font-normal'>
+                {tweet.text}
+              </p>
+              <div className='w-9/12 mt-3 flex justify-between items-center'>
+                <TweetButton
+                  onClick={() => {
+                    handlers((v) => !v);
+                  }}
+                  Icon={FaRegComment}
+                  count={tweet.comments}
+                />
+                <TweetButton
+                  onClick={handleRetweetButton}
+                  Icon={AiOutlineRetweet}
+                  count={tweet.retweets}
+                  active={tweet.isRetweeted}
+                  ActiveIcon={AiOutlineRetweet}
+                  color='green'
+                />
+                <TweetButton
+                  onClick={handleLikeButton}
+                  active={tweet.isLiked}
+                  ActiveIcon={AiFillHeart}
+                  Icon={AiOutlineHeart}
+                  count={tweet.likes}
+                  color='red'
+                />
+                <TweetButton onClick={() => {}} Icon={AiOutlineShareAlt} />
+              </div>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -232,7 +231,7 @@ function TweetButton({
       {formatted_count && (
         <span
           className={`${
-            active ? col[2] : 'text-textgray'
+            hovered ? col[2] : 'text-textgray'
           } text-xs  transition-colors`}
         >
           {formatted_count}
@@ -242,122 +241,11 @@ function TweetButton({
   );
 }
 
-function TweetOptions({
-  id,
-  isBookmarked,
-  isAuthor,
-  open,
-  reference,
-  setOpen
-}: TweetOptionsProps) {
-  const bookmark = useMutation({
-    mutationFn: BookmarkTweet,
-    onSuccess: (_data, tweetId) => {
-      queryClient.setQueryData<LatestTweetsType | undefined>(
-        ['Tweet', tweetId],
-        (data) => {
-          if (!data) return undefined;
-          return {
-            ...data,
-            isBookmarked: true
-          };
-        }
-      );
-      queryClient.invalidateQueries(['Tweet'], { exact: true });
-      queryClient.invalidateQueries(['Bookmark'], { exact: true });
-    }
-  });
-  const unbookmark = useMutation({
-    mutationFn: UnBookmarkTweet,
-    onSuccess: (_data, tweetId) => {
-      queryClient.setQueryData<LatestTweetsType | undefined>(
-        ['Tweet', tweetId],
-        (data) => {
-          if (!data) return undefined;
-          return {
-            ...data,
-            isBookmarked: false
-          };
-        }
-      );
-      queryClient.invalidateQueries(['Tweet'], { exact: true });
-      queryClient.invalidateQueries(['Bookmark'], { exact: true });
-    }
-  });
-
-  const delete_tweet = useMutation({
-    mutationFn: DeleteTweet,
-    onSuccess: () => {
-      queryClient.invalidateQueries(['Tweet'], { exact: true });
-      queryClient.invalidateQueries(['Bookmark'], { exact: true });
-    }
-  });
-
-  const handleBookmark = async () => {
-    try {
-      if (!isBookmarked) await bookmark.mutateAsync(id);
-      else await unbookmark.mutateAsync(id);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleDeleteTweet = async () => {
-    try {
-      await delete_tweet.mutateAsync(id);
-    } catch (err: any) {
-      console.log(err);
-    }
-  };
-  return (
-    <Dropdown
-      reference={reference}
-      open={open}
-      setOpen={setOpen}
-      className='shadow shadow-gray-500 rounded-lg py-2.5 bg-black w-fit flex flex-col'
-    >
-      <TweetOptionButton onClick={handleBookmark}>
-        {isBookmarked ? (
-          <>
-            <BiBookmarkMinus className='text-lg fill-red-500' />
-            <p className='text-sm font-bold text-red-500'>Unbookmark</p>
-          </>
-        ) : (
-          <>
-            <BiBookmarkPlus className='text-lg' />
-            <p className='text-sm font-bold'>Add Bookmark</p>
-          </>
-        )}
-      </TweetOptionButton>
-      {isAuthor && (
-        <TweetOptionButton onClick={handleDeleteTweet}>
-          <FiDelete className='text-lg' />
-          <p className='text-sm font-bold'>Delete tweet</p>
-        </TweetOptionButton>
-      )}
-    </Dropdown>
-  );
-}
-
-function TweetOptionButton({ onClick, children }: TweetOptionButtonProps) {
-  return (
-    <button
-      className='text-md mt-2 hover:bg-search py-0.5 px-4 w-full'
-      onClick={onClick}
-    >
-      <div className='flex gap-3 items-center text-base'>{children}</div>
-    </button>
-  );
-}
-
-function CommentModal() {}
-
 TweetCardV3.defaultProps = {
   img: './defaultpfp.png',
-  retweets: '',
-  likes: '',
-  comments: '',
-  bookmarked: false,
+  retweets: 0,
+  likes: 0,
+  comments: 0,
   showRetweets: false
 };
 
