5ae83bdbf9c5acb5752a64df91fcacec2cb38b19
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2023-04-26 10:33:16 -0700

    fix: import LatestTweetsController

--- server/src/controllers/tweet/index.ts
+++ server/src/controllers/tweet/index.ts
@@ -1,5 +1,5 @@
 export { default as createTweetController } from './create.controller';
-export { default as latestTweetsController } from './latest.controller';
+export { default as latestTweetsController } from './explore.controller';
 export { default as UserTweetsController } from './user.controller';
 export { default as deleteTweetController } from './delete.controller';
 export { default as LikeController } from './likes/like.controller';
@@ -8,3 +8,4 @@ export { default as RetweetController } from './retweet/retweet.controller';
 export { default as UnRetweetController } from './retweet/unretweet.controller';
 export { default as CommentController } from './comment/addComment.controller';
 export { default as UnCommentController } from './comment/removeComment.controller';
+export { default as IdController } from './id.controller';
