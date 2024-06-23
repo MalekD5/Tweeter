7bf130d2f5970b4234c064ff9e0662e0d65fc17c
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-24 01:20:17 +0300

    feat: add logout button in navbar user component

--- src/app/(layout)/_components/user.tsx
+++ src/app/(layout)/_components/user.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { auth } from '@/lib/auth';
+import { Button } from '@/components/ui/button';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { auth, signOut } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import { IoIosMore } from 'react-icons/io';
 
@@ -10,21 +12,35 @@ export default async function User() {
   if (!session?.user) return redirect('/');
 
   return (
-    <div className="flex w-full cursor-pointer items-center justify-between gap-4 rounded-full p-3 hover:bg-zinc-700">
-      <img
-        src={session.user?.image!}
-        className="rounded-full"
-        alt="user logo"
-        width={40}
-        height={40}
-      />
-      <div className="hidden flex-col lg:flex">
-        <p className="text-sm">{session.user?.name}</p>
-        <p className="text-sm text-zinc-700">@{session.user?.name}</p>
-      </div>
-      <div className="hidden grow basis-auto justify-end lg:flex">
-        <IoIosMore />{' '}
-      </div>
-    </div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <div className="flex w-full cursor-pointer items-center justify-between gap-4 rounded-full p-3 hover:bg-zinc-700">
+          <img
+            src={session.user?.image!}
+            className="rounded-full"
+            alt="user logo"
+            width={40}
+            height={40}
+          />
+          <div className="hidden flex-col lg:flex">
+            <p className="text-sm">{session.user?.name}</p>
+            <p className="text-sm text-zinc-700">@{session.user?.name}</p>
+          </div>
+          <div className="hidden grow basis-auto justify-end lg:flex">
+            <IoIosMore />{' '}
+          </div>
+        </div>
+      </PopoverTrigger>
+      <PopoverContent className="w-60">
+        <form
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
+        >
+          <Button variant="ghost">Logout from @{session.user?.name}</Button>
+        </form>
+      </PopoverContent>
+    </Popover>
   );
 }
