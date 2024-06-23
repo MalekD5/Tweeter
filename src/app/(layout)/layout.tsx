9111bc4fc30a03a9de3aa04233e721bef071c317
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-24 00:42:12 +0300

    style: make grid and post button responsive in navbar

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -7,10 +7,12 @@ import User from './_components/user';
 import NavContainer from './_components/nav/nav-container';
 import NavItem from './_components/nav/nav-item';
 import { Page } from './_context/types';
+import { BsFeather } from 'react-icons/bs';
+import { LiaFeatherAltSolid } from 'react-icons/lia';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid min-h-screen grid-cols-[0.8fr_1fr_1fr] grid-rows-1">
+    <div className="grid min-h-screen grid-cols-[100px_1fr] grid-rows-1 lg:grid-cols-[0.8fr_1fr_1fr]">
       <header className="relative col-start-1 col-end-2 flex w-full justify-end border-r border-zinc-700">
         <div className="w-4/6 pr-2">
           <div className="relative">
@@ -30,13 +32,9 @@ export default async function Layout({ children }: { children: React.ReactNode }
                 icon={<IoSettingsOutline />}
                 active={<IoSettingsSharp />}
               />
-              <Button
-                className="text-md mt-2 w-5/6 font-bold"
-                size="lg"
-                rounded="full"
-                variant="blue"
-              >
-                Post
+              <Button className="text-md mt-2 py-0 font-bold lg:py-5" rounded="full" variant="blue">
+                <p className="hidden lg:block">Post</p>
+                <LiaFeatherAltSolid className="block size-20 lg:hidden" />
               </Button>
             </NavContainer>
             <div className="absolute bottom-2 w-full">
@@ -45,7 +43,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
           </div>
         </div>
       </header>
-      <main className="col-start-2 col-end-4">{children}</main>
+      <main className="col-start-2 col-end-3 lg:col-end-4">{children}</main>
     </div>
   );
 }
