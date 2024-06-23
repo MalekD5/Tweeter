acd83abd9ca3345a27457cff645c49927a11d664
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-24 00:43:29 +0300

    style: make user in navbar responsive

--- src/app/(layout)/_components/user.tsx
+++ src/app/(layout)/_components/user.tsx
@@ -11,12 +11,18 @@ export default async function User() {
 
   return (
     <div className="flex w-full cursor-pointer items-center justify-between gap-4 rounded-full p-3 hover:bg-zinc-700">
-      <img src={session.user?.image!} alt="user logo" width={32} height={32} />
-      <div className="flex flex-col">
+      <img
+        src={session.user?.image!}
+        className="rounded-full"
+        alt="user logo"
+        width={40}
+        height={40}
+      />
+      <div className="hidden flex-col lg:flex">
         <p className="text-sm">{session.user?.name}</p>
         <p className="text-sm text-zinc-700">@{session.user?.name}</p>
       </div>
-      <div className="flex grow basis-auto justify-end">
+      <div className="hidden grow basis-auto justify-end lg:flex">
         <IoIosMore />{' '}
       </div>
     </div>
