5273a6d401d698d651fd4b6b15a4fbd8cb00c5ad
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-24 00:03:04 +0300

    refactor: restructure app router

--- src/app/(layout)/(pages)/home/page.tsx
+++ src/app/(layout)/(pages)/home/page.tsx
@@ -1,5 +1,4 @@
 import { auth } from '@/lib/auth';
-import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
@@ -8,5 +7,6 @@ export default async function Home() {
   if (!session) {
     return redirect('/');
   }
+
   return <div>Home page</div>;
 }
