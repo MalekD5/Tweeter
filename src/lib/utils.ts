111ec8634f1cf56e3e35d94dcef8f6dd838a2ae8
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-23 22:46:05 +0300

    refactor: add capitalize function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function capitalize<T extends string>(s: T) {
+  return (s[0].toUpperCase() + s.slice(1)) as Capitalize<typeof s>;
+}
