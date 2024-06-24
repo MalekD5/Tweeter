99586ac6e651299376c62dfcf3d1149005273823
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-24 16:41:40 +0300

    fix: middleware is running on routes that should be ignored the middleware is running on routes that it shouldn't run on like the root route `/` and `/logo.svg` route `complete-signup` route has been added preparing for proper sign up implementation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,18 @@
-export { auth as middleware } from '@/lib/auth';
+import { auth } from '@/lib/auth';
+import { NextResponse } from 'next/server';
+
+export default auth((req) => {
+  if (!req.auth) {
+    return NextResponse.redirect(new URL('/', req.nextUrl.origin));
+  }
+
+  const { auth } = req;
+  if (!auth.user.username) {
+    return NextResponse.redirect(new URL('/complete-signup', req.nextUrl.origin));
+  }
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico|logo.svg|complete-signup|$).*)'],
 };
