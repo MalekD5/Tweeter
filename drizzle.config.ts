9553a7e4315732aa0b71663a9902cddae6e271cb
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-24 16:30:16 +0300

    chore: fix schema path for drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { Config, defineConfig } from 'drizzle-kit'
 
 // @ts-ignore
 export default defineConfig({
- schema: "./src/db/schemas.ts",
+ schema: "./src/lib/db/schemas.ts",
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL,
