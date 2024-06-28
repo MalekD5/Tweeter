72f35d230e958a51610f46b00f32a9ad6f3ce652
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-28 18:10:22 +0300

    refactor: add tweet and tweetEntity schemas

--- src/lib/db/schemas.ts
+++ src/lib/db/schemas.ts
@@ -1,10 +1,10 @@
-import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { boolean, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
-export const users = pgTable("user", {
+export const usersTable = pgTable("user", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  google_id: text("google_id").unique(),
+  googleId: text("google_id").unique(),
   name: text("name"),
   username: text("username").unique(),
   dateOfBirth: timestamp("dateofbirth", { mode: "date" }),
@@ -15,4 +15,32 @@ export const users = pgTable("user", {
   image: text("image"),
 });
 
-export type DBUser = typeof users.$inferSelect;
+export const tweetsTable = pgTable("tweet", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  userId: text("user_id")
+    .notNull()
+    .references(() => usersTable.id),
+  text: text("tweet_text").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  hasEntities: boolean("has_entities").notNull().default(false),
+});
+
+export const entityTable = pgTable("tweet_entity", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  tweetId: text("tweet_id")
+    .notNull()
+    .references(() => tweetsTable.id),
+  entityType: pgEnum("entity_type", ["image", "video", "user"]) as any,
+  entityUrl: text("entity_url").notNull(),
+});
+
+export type DBUser = typeof usersTable.$inferSelect;
+export type DBUserInsert = typeof usersTable.$inferInsert;
+export type DBTweet = typeof tweetsTable.$inferSelect;
+export type DBTweetInsert = typeof tweetsTable.$inferInsert;
+export type DBEntity = typeof entityTable.$inferSelect;
+export type DBEntityInsert = typeof entityTable.$inferInsert;
