f556a5d4c580c80b89a0d9881360df7c5061d006
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-29 21:07:06 +0300

    [refactor] for-you-tab: add PostTweet and FyInfiniteScrolling components

--- src/app/(layout)/(pages)/home/_components/for-you-tab.tsx
+++ src/app/(layout)/(pages)/home/_components/for-you-tab.tsx
@@ -1,5 +1,16 @@
+import PostTweet from "@/components/tweet/post-tweet";
 import { TabsContent } from "@/components/ui/tabs";
+import useInfiniteScrolling from "@/hooks/use-infinite-scrolling";
+import { useState } from "react";
+import FyInfiniteScrolling from "./fy-infinite-scrolling";
 
 export default function FyTab() {
-  return <TabsContent value="fy">For you tab</TabsContent>;
+  return (
+    <TabsContent value="fy">
+      <div className="flex flex-col">
+        <PostTweet />
+        <FyInfiniteScrolling />
+      </div>
+    </TabsContent>
+  );
 }
