b4128e01c701b19a45de84bde5996a21b3c3bc04
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-29 19:25:51 +0300

    feat: uploadthing core and route handler

---
+++ src/app/api/upload/core.ts
@@ -0,0 +1,29 @@
+import { getSession } from "@/actions/auth";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const fileRouter = {
+  imageUploader: f({
+    image: { maxFileCount: 4, maxFileSize: "2MB" },
+    video: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async () => {
+      const session = await getSession();
+      if (!session.id || !session.username) {
+        throw new UploadThingError("Unauthorized");
+      }
+
+      return { userId: session.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => ({
+      uploadedBy: metadata.userId,
+      fileId: file.key,
+    })),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof fileRouter;
