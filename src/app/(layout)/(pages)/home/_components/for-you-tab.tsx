ea8ed95d04860c4a3a9150f3060257f9e1741ebf
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-29 21:22:30 +0300

    [refactor] for-you-tab: remove unused imports

--- src/app/(layout)/(pages)/home/_components/for-you-tab.tsx
+++ src/app/(layout)/(pages)/home/_components/for-you-tab.tsx
@@ -1,7 +1,5 @@
 import PostTweet from "@/components/tweet/post-tweet";
 import { TabsContent } from "@/components/ui/tabs";
-import useInfiniteScrolling from "@/hooks/use-infinite-scrolling";
-import { useState } from "react";
 import FyInfiniteScrolling from "./fy-infinite-scrolling";
 
 export default function FyTab() {
