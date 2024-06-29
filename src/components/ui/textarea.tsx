ea81ba97e16eaacb17dd5daf299b4ad711eeaca7
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-06-29 21:02:50 +0300

    feat: textarea auto height resize

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,23 +1,34 @@
+"use client";
+
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  autosize?: boolean;
+}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, autosize = false, ...props }, ref) => {
     return (
       <textarea
         className={cn(
           "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          className,
         )}
+        onInput={
+          autosize
+            ? (e) => {
+                e.currentTarget.style.height = `0px`;
+                e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
+              }
+            : undefined
+        }
         ref={ref}
         {...props}
       />
     );
-  }
+  },
 );
 Textarea.displayName = "Textarea";
 
